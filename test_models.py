709d426c85b877a1740ac240047077e43201601c
unknown <dulce.1201@gmail.com>
2022-12-19 15:48:38 +0100

    modificando test_models

--- test_models.py
+++ test_models.py
@@ -13,8 +13,9 @@ def test_cambio_ok():
 	cambio = Exchange("ETC")
 	assert cambio.rate is None#True
 	assert cambio.time is None#True
-	cambio.updateExchange(api_Key)
-	assert cambio.rate > 0
+	cambio.updateExchange(api_Key) #rate y time deberían después de hacer un llamdo exitoso
+	assert cambio.rate > 0 #Que el valor de la  coin sea mayor a 0 si la llamada a la api va bien
+	print(cambio.rate)
 	assert isinstance(cambio.time,str)
 
 def test_cambio_no_ok():
